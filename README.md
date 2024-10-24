03fb4c8d8194980b8ff2a10103d83d4d94c1b3c0
Lcoderloc454 <Williamjackson2734@my.whccd.edu>
2024-10-23 20:00:40 -0700

    Added a Hello World! Whats good? and added pi = 34 error that it did run before putting the //

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # COMP002Lecture.Activity2-1
+Author: William Jackson
+Purpose: COMP002 - Lecture Activity 2-1 - Expression Statement and Variables
---
+++ script.js
@@ -0,0 +1,22 @@
+
+//This is an example of how to do it.
+
+// string variable
+let myVariable = "Hello World!"
+console.log(myVariable)
+
+// change the string variable value
+myVariable = "I changed the value of the string variable here.";
+console.log(myVariable);
+
+//const variable
+const pi = 3.14;
+//pi = 2 //throws an error
+
+let myGreeting = "Hello World! Whats good?"
+console.log(myGreeting) //result: Hello World! Whats good?
+
+//pi = 34// error
+
+let myNumber = 34;
+console.log(Math.min(myNumber, pi)); // result: 3.14
